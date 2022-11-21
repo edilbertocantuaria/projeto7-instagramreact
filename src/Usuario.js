bf012b872f353924635f9b39481c3fb4ed287059
Edilberto Cantuaria <edilbertocantuaria@users.noreply.github.com>
2022-11-21 12:52:19 -0300

    Criei os imports/exports
    Criei os imports/exports, agora falta resolver o bug das imagens e formatação do css. Creio que é o caminho que tá errado.

---
+++ src/Usuario.js
@@ -0,0 +1,14 @@
+export default function Usuario(){
+    return(
+        <div className="usuario">
+        <img src="public/assets/img/catanacomics.svg" />
+        <div className="texto">
+          <strong>catanacomics</strong>
+          <span>
+            Catana
+            <ion-icon name="pencil"></ion-icon>
+          </span>
+        </div>
+      </div>
+    )
+}
