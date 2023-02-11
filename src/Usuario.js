677ef0bacfcd50819c4fd96df30fb13af6ee57b1
edilbertocantuaria <edilbertounbfga@gmail.com>
2023-02-11 11:44:29 -0300

    Dados dinâmicos do usuário: agora as informações são passadas via props e é possível editar a imagem e nome!

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,17 +1,51 @@
-export default function Usuario(){
-    return(
-      <div class="meuPerfil">
-      <div class="espHoriz espEsqPubli2">
+import React from "react";
+
+function InfoUsuario(props){
+  const [arrobaUser, setArrobaUser] = React.useState(props.arrobaUser)
+  const [imgPerfil, setImgPerfil] = React.useState(props.imgPerfil)
+
+
+  function editarArrobaUser(){
+    let novoArroba = prompt("Digite seu novo @")
+
+    if (novoArroba ==="" || novoArroba ===null || novoArroba===undefined){
+     
+    } else {
+      setArrobaUser(novoArroba)
+    }
+
+}
+
+function editarImgUser(){
+  let novaImg = prompt("Deixe o link da nova imagem:")
+
+  if (novaImg ==="" || novaImg ===null || novaImg===undefined){
+   
+  } else {
+    setImgPerfil(novaImg)
+  }
+
+}
+
+return (
+  <div class="espHoriz espEsqPubli2">
         <img data-test="profile-image"
-          src="assets/imgs/quemPublicou/edilbertices_perfil.jpg"
+          src={imgPerfil}
           class="fotoPerfilSB"
-        />
+          onClick={() => editarImgUser()} />
         <div class="espDirPubli centVert2 publi">
-          <div class="user-icon espHoriz2"><h1 data-test="name" >edilbertices</h1> <img class="pen-edit" data-test="edit-name" src="assets/Edit_fill.svg"/></div>
-          <h2>Beto Cantuária</h2>
+          <div class="user-icon espHoriz2"><h1 data-test="name" >{arrobaUser}</h1> <img class="pen-edit" data-test="edit-name" src="assets/Edit_fill.svg" onClick={() => editarArrobaUser()} /></div>
+          <h2>{props.nomeUsuario}</h2>
           
         </div>
       </div>
+)
+}
+
+export default function Usuario(){
+    return(
+      <div class="meuPerfil">
+        <InfoUsuario imgPerfil="assets/imgs/quemPublicou/edilbertices_perfil.jpg" arrobaUser="edilbertices" nomeUsuario="Beto Cantuária" />
     </div>
     )
 }
